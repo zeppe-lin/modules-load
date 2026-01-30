2c76f1c8ac1ab4bf2f107ba2c14d23b7fa4f809d
Alexandr Savca <alexandr.savca89@gmail.com>
2026-01-30 18:04:44 +0200

    chore(config.mk): bump version 0.1 -> 0.2

--- config.mk
+++ config.mk
@@ -1,7 +1,8 @@
 # project metadata
 NAME        = modules-load
-VERSION     = 0.1
+VERSION     = 0.2
 
 # paths
 PREFIX      = /usr
 MANPREFIX   = $(PREFIX)/share/man
+BASHCOMPDIR = $(PREFIX)/share/bash-completion/completions
